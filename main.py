2ef7b973bde0eac2c62e97a053f91ce636197b81
NguyenDat99 <donguyen5548@gmail.com>
2019-03-31 19:16:18 +0700

    Cap Nhat

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 # coding=utf-8
-import csv
-import numpy as np
 import pandas as pd
 from pyvi import ViTokenizer, ViPosTagger
 
 data=[]
 DauVao=pd.read_csv('./data/training/TrainingData.csv',encoding='utf-8')
-data.append([(DauVao['DauVao'].values).tolist(),(DauVao['Nhan'].values).tolist()])
-data=np.array(data)
+x_training=(DauVao['DauVao'].values).tolist()
+y_Training=(DauVao['Nhan'].values).tolist()
 
-#print(ViPosTagger.postagging(ViTokenizer.tokenize(u"%s" %data[:,0])))
-#X=ViPosTagger.postagging(ViTokenizer.tokenize(u"tôi đang sống chốn nào thế " ))
-from sklearn.feature_extraction import DictVectorizer
-X=[{'a':'1'},{'b':'2'},{'c':'3'},{'d':'1'},{'e':'3'}]
-vec = DictVectorizer()
-print(vec.fit_transform(X).toarray())
+for i in range(len(x_training)):
+    k =ViPosTagger.postagging(ViTokenizer.tokenize(u"%s" %x_training[i]))
+    data.append([k[0],k[1]])
+
+print(data[2])
+
+#from sklearn.feature_extraction import DictVectorizer
+#X=[{'a':'N'},{'b':'3'},{'c':'3'},{'d':'3'}]
+#vec = DictVectorizer()
+#print(vec.fit_transform(data).toarray())
