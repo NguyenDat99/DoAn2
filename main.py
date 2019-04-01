ac29ecbeb05df4859e15df5dc10fb3eb39e58ced
NguyenDat99 <donguyen5548@gmail.com>
2019-04-01 10:42:10 +0700

    .

--- main.py
+++ main.py
@@ -9,9 +9,16 @@ y_Training=(DauVao['Nhan'].values).tolist()
 
 for i in range(len(x_training)):
     k =ViPosTagger.postagging(ViTokenizer.tokenize(u"%s" %x_training[i]))
-    data.append([k[0],k[1]])
+    j=len(k[1])-1
+    while j>=0:
+        if not (k[1][j]=='N' or k[1][j]=='A'):
+            del(k[1][j])
+            del(k[0][j])
+        j-=1
+    print(k[0])
+    print(k[1])
+    #data.append([k[0],k[1]])
 
-print(data[5])
 
 #from sklearn.feature_extraction import DictVectorizer
 #X=[{'a':'N'},{'b':'3'},{'c':'3'},{'d':'3'}]
