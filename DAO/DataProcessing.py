739e00474b250f6e173129015920d33fad5965e0
Sasa <sa@M.local>
2019-05-21 10:54:14 +0700

    TF-IDF

---
+++ DAO/DataProcessing.py
@@ -0,0 +1,117 @@
+# coding=utf-8
+import sys
+sys.path.append('../DTO/')
+import pandas as pd
+import DataAdapter as da
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+from pyvi import ViTokenizer, ViPosTagger
+data=da.dataset()
+
+def loaiTru(s):
+    for i in s:
+        if i =='.' or i=='?' or i==',' or i=='0' or i==' ' or i=='1' or i=='2' or i=='3' or i=='4' or i=='5' or i=='6' or i=='7'or i=='8' or i=='9' or i==')'or i=='('or i=='"'or i=='='or i=='>'or i=='<'or i=='&'or i=='^'or i=='*'or i=='%'or i=='#'or i=='$'or i==''or i=='@'or i==':' or i==';':
+            return 0
+    return 1
+
+def locKiTuDacBiet(s):
+    s1=""
+    for i in s:
+        if i!='!' and i !='/' :
+            s1+=i
+    return s1
+
+def lamSachChuoi():
+    dt=data.text
+    for i in range(len(dt)):
+        s=""
+        k=dt[i].split(" ")
+        for j in k:
+            if loaiTru(j)==1:
+                s+=locKiTuDacBiet(j)+" "
+        dt[i]=s
+    return dt
+
+def demTu(dt):
+    k =dt.split(" ")
+    dem=0
+    for i in k:
+        if i !=" ":
+            dem+=1
+    return dem
+
+def bag_of_words(dt):
+    tuDien=[]
+    s=[]
+    s1=[]
+    # cat chuoi tung cau noi lai
+    for i in range(len(dt)):
+        k=dt[i].split(" ")
+        for j in k:
+            if j!=' ':
+                s.append(j)
+    #loc trung
+    for i in range(len(s)):
+        if s[i] not in s1:
+            s1.append(s[i])
+    #bo tu dien
+    for i in range(len(dt)):
+        array=[]
+        k=dt[i].split(" ")
+        for j in s1:
+            dem=0
+            if j not in k:
+                array.append([j,0])
+            else:
+                for h in k:
+                    if j ==h:
+                        dem+=1
+                array.append([j,dem])
+        tuDien.append(np.array(array))
+    return tuDien
+
+def tinhTF(dt,tuDien):
+    array=[]
+    tongTu=demTu(dt)
+    for tu, chiSo in tuDien:
+        if tu!=' ':
+            array.append([tu,float(chiSo)/tongTu])
+    return np.array(array)
+
+def tinhIDF(dt,tuDien):
+    array=[]
+    tongCau=len(dt)
+    dem=0
+    for tu, chiSo in tuDien[0]:
+        for cau in dt:
+            if tu in cau.split():
+                dem+=1
+        if(dem>0):
+            array.append([tu,float(tongCau)/dem])
+        dem=0
+    return np.array(array)
+
+def tinhTF_IDF(tuDienTF,tuDienIDF):
+    array=[]
+    for tu_tf, chiSo_tf in tuDienTF:
+        for tu_idf, chiSo_idf  in tuDienIDF:
+            if tu_tf==tu_idf:
+                array.append([tu_tf,float(chiSo_tf)*float(chiSo_idf)])
+    return np.array(array)
+def tuDien():
+    dt=lamSachChuoi()
+    tuDien=bag_of_words(dt)
+    tuDienTF=tuDien
+    tuDienIDF=[]
+    tuDienTF_IDF=tuDien
+    #tinhTF
+    tuDienTF=tuDien
+    for i in range(len(dt)):
+        tuDienTF[i]=tinhTF(dt[i],tuDien[i])
+    #tinhIDF
+    tuDienIDF=tinhIDF(dt,tuDien)
+    #tinhTF_IDF
+    #for i in range(len(tuDienTF)):
+        #tuDienTF_IDF[i]=tinhTF_IDF(tuDienTF[i],tuDienIDF)
+    print(tinhTF_IDF(tuDienTF[0],tuDienIDF))
+tuDien()
--- DTO/DataAdapter.py
+++ DTO/DataAdapter.py
@@ -1,23 +1,31 @@
 # coding=utf-8
 import pandas as pd
-
-csv1=pd.read_csv('../DTO/dataset/CSV/data1.csv',encoding='utf-8')
-csv2=pd.read_csv('../DTO/dataset/CSV/data2.csv',encoding='utf-8')
+csv=pd.read_csv('../DTO/dataset/CSV/data.csv',encoding='utf-8')
+from multiprocessing.dummy import Pool as ThreadPool
 
 def getFeature(file,name):
     return (file[name].values).tolist()
-def getFeature(file):
-    return (file.values).tolist()
+# def getFeature(file):
+#     return (file.values).tolist()
+def getFeature(name):
+    return (csv[name].values).tolist()
+
+
+def multiprocessing():
+    t=['text','label']
+    pool = ThreadPool(2)
+    data=pool.map(getFeature,t)
+    return data
 
 def ExCSV():
     array=[]
-    for i in range(1,35473):
-        try:
-            PosFile1 = pd.read_csv('../DTO/dataset/data_train/pos/%s.txt'%i, sep=" ", header=None)
-            if PosFile1!=None:
-                array.append([getFeature(PosFile1),1])
-        except:
-            a=2
+    # for i in range(1,300): #35473
+    #     try:
+    #         PosFile1 = pd.read_csv('../DTO/dataset/data_train/pos/%s.txt'%i, sep=" ", header=None)
+    #         print(PosFile1[0])
+    #         array.append([getFeature(PosFile1),1])
+    #     except:
+    #         a=2
     # for i in range(1,29244):
     #     try:
     #         PosFile1 = pd.read_csv('../DTO/dataset/data_train/neg/%s.txt'%i, sep=" ", header=None)
@@ -39,10 +47,9 @@ def ExCSV():
     #             array.append([getFeature(PosFile1),0])
     #     except:
     #         a=2
-    return array
+    return  array
 class dataset:
-    def __init__(seft,text,label):
-        seft.text=text
-        seft.label=label
-
-print(ExCSV())
+    def __init__(seft):
+        data=multiprocessing()
+        seft.text=data[0]
+        seft.label=data[1]
